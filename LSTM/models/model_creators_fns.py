f4b3325a769022595a967b7055a12050dc6f5b14
RanGreidi <rangrudi@gmail.com>
2024-04-24 17:57:30 +0300

    minors

---
+++ LSTM/models/model_creators_fns.py
@@ -0,0 +1,87 @@
+
+import tensorflow as tf
+import tensorflow_federated as tff
+import data_handler.data_fuctions as data_fuctions
+from src.FLARE_regulerizer import FLARE_REGULARIZATION
+from src.FedProx_regulerizer import FedProx_REGULARIZATION
+import data_handler.data_fuctions as data
+#from src.general_utils import *
+# %%
+def create_keras_model():
+    initializer = tf.keras.initializers.GlorotNormal#(seed=0)
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=data_fuctions.Input_shape),
+        tf.keras.layers.Dense(4069, kernel_initializer=initializer),
+        #tf.keras.layers.Dense(4069, kernel_initializer=initializer),
+        #tf.keras.layers.Dense(4069, kernel_initializer=initializer),
+        tf.keras.layers.Dense(10, kernel_initializer=initializer),
+        tf.keras.layers.Softmax(),
+    ])
+def create_keras_model_for_FLARE(accumolator,server_weights,tau,u):
+      keras_model = create_keras_model() 
+      new_input_layer = tf.keras.layers.Input(shape=data_fuctions.Input_shape)
+      x = new_input_layer
+      i = 0
+      for layer in keras_model.layers:
+        config = layer.get_config()
+        new_layer = tf.keras.layers.deserialize({'class_name':layer.__class__.__name__,'config': config})
+
+        if new_layer.trainable:
+          if hasattr(new_layer, 'kernel_regularizer'):
+              #print("acc shape: ",accumolator.trainable[2*i].shape)
+              #print("layer kernel shape: ",new_layer.output_shape)          
+              new_layer.kernel_regularizer = FLARE_REGULARIZATION(tau,u,accumolator.trainable[2*i],server_weights.trainable[2*i])
+          if hasattr(new_layer, 'bias_regularizer'):
+              #print("acc shape: ",accumolator.trainable[2*i+1].shape)
+              #print("layer bias shape: ",new_layer.bias.shape)
+              new_layer.bias_regularizer = FLARE_REGULARIZATION(tau,u,accumolator.trainable[2*i+1],server_weights.trainable[2*i+1])
+              i +=1
+        x = new_layer(x)
+      
+      reg_model = tf.keras.models.Model(inputs=new_input_layer, outputs=x)
+      return reg_model
+def create_keras_model_for_FedProx(server_weights,tau,u):
+      keras_model = create_keras_model() 
+      new_input_layer = tf.keras.layers.Input(shape=data_fuctions.Input_shape)
+      x = new_input_layer
+      i = 0
+      for layer in keras_model.layers:
+        config = layer.get_config()
+        new_layer = tf.keras.layers.deserialize({'class_name':layer.__class__.__name__,'config': config})
+
+        if new_layer.trainable:
+          if hasattr(new_layer, 'kernel_regularizer'):
+              #print("acc shape: ",accumolator.trainable[2*i].shape)
+              #print("layer kernel shape: ",new_layer.output_shape)          
+              new_layer.kernel_regularizer = FedProx_REGULARIZATION(tau,u,server_weights.trainable[2*i])
+          if hasattr(new_layer, 'bias_regularizer'):
+              #print("acc shape: ",accumolator.trainable[2*i+1].shape)
+              #print("layer bias shape: ",new_layer.bias.shape)
+              new_layer.bias_regularizer = FedProx_REGULARIZATION(tau,u,server_weights.trainable[2*i+1])
+              i +=1
+        x = new_layer(x)
+      
+      reg_model = tf.keras.models.Model(inputs=new_input_layer, outputs=x)
+      return reg_model
+# %%
+def model_fn_for_clients(accumolator,server_weights,tau,u):
+    keras_model = create_keras_model_for_FLARE(accumolator,server_weights,tau,u)           
+    return tff.learning.from_keras_model(
+                                keras_model,
+                                input_spec=data.input_spec,
+                                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])        
+def model_fn_for_FedProx(server_weights,tau,u):
+    keras_model = create_keras_model_for_FedProx(server_weights,tau,u)           
+    return tff.learning.from_keras_model(
+                                keras_model,
+                                input_spec=data.input_spec,
+                                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])   
+def model_fn():
+  keras_model = create_keras_model()
+  return tff.learning.from_keras_model(
+                                keras_model,
+                                input_spec=data.input_spec,
+                                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
---
+++ LSTM/utils/log_writer.py
@@ -0,0 +1,62 @@
+import os
+def log_writer(experiment_name,
+                history_federeted,
+                history_FedAvg,
+                history_second_algo_server_state,
+                history_third_algo_server_state,
+                history_fourth_algo_server_state,
+                prun_precent_logger,
+                E_logger,
+                prun_precent_logger_FFL,
+                E_logger_FFL,
+                output_list, a,b,c,d,e,round):
+
+    #write FLARE loss to text file
+    with open(os.path.join('results/' + experiment_name, 'FLARE_evaluation'), "w") as FedAlgo_file:
+        toFile = "\n".join(str(item) for item in history_federeted)
+        FedAlgo_file.write(toFile)
+    #write FedAvg loss to text file
+    with open(os.path.join('results/' + experiment_name, 'FedAvg_evaluation'), "w") as Fedavg_file:
+        toFile = "\n".join(str(item) for item in history_FedAvg)
+        Fedavg_file.write(toFile)  
+    #write Second Algo loss to text file
+    with open(os.path.join('results/' + experiment_name, 'Error_Coreection_evaluation'), "w") as Second_FedAlgo_file:
+        toFile = "\n".join(str(item) for item in history_second_algo_server_state)
+        Second_FedAlgo_file.write(toFile)   
+    #write Third Algo loss to text file
+    with open(os.path.join('results/' + experiment_name, 'Error_Coreection_evaluation'), "w") as Third_FedAlgo_file:
+        toFile = "\n".join(str(item) for item in history_third_algo_server_state)
+        Third_FedAlgo_file.write(toFile) 
+
+    #write prun_precent_logger to text file
+    with open(os.path.join('results/' + experiment_name, 'FLARE_Sparse_precent_logger'), "w") as outputfile:
+        toFile = "\n".join(str(item) for item in prun_precent_logger)
+        outputfile.write(toFile)
+
+    #write E_logger to text file
+    with open(os.path.join('results/' + experiment_name, 'FLARE_E_logger'), "w") as outputfile:
+        toFile = "\n".join(str(item) for item in E_logger)
+        outputfile.write(toFile)
+
+    #write FFL prun_precent_logger to text file
+    with open(os.path.join('results/' + experiment_name, 'SecondAlgo_Sparse_precent_logger'), "w") as outputfile:
+        toFile = "\n".join(str(item) for item in prun_precent_logger_FFL)
+        outputfile.write(toFile)
+
+    #write FFL E_logger to text file
+    with open(os.path.join('results/' + experiment_name, 'SecondAlgo_E_logger'), "w") as outputfile:
+        toFile = "\n".join(str(item) for item in E_logger_FFL)
+        outputfile.write(toFile)
+
+    #write output list for linux terminal
+    output_list.append(str(
+                        "round: " + str(round) + '\n'
+                        'FLARE evaluation:   ' + str(a[1]) + '\n'
+                        'FedAvg evaluation:   '        + str(b[1]) + '\n'
+                        'second_algo_server_state:   '     + str(c[1]) + '\n'
+                        'third_algo_server_state:   '     + str(d[1]) + '\n' +'\n'
+                        ))
+    with open(os.path.join('results/' + experiment_name, 'output'), "w") as outputfile:
+        toFile = "\n".join(str(item) for item in output_list)
+        outputfile.write(toFile)
+    #print('\n')
